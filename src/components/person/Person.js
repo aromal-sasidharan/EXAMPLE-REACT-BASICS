b8b5d7beab56ffe72964157db5c660e45826c996
Aromal <aromalsasidharan@gmail.com>
2018-07-24 15:07:04 +0530

    sample higher order component

--- src/components/person/Person.js
+++ src/components/person/Person.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './Person.css';
-
+import wrapperWithClass from '../hoc/wrapperWithClass'
+import Aux from '../hoc/Aux'
 class Person extends React.Component{
 
     render() {
         console.log("Person.js render");
         return (
-            <div className={styles.Person}>
+            <Aux>
                 <p>I'am <b onClick={this.props.onDeleteClicked}>{this.props.name}</b> and i'am <b>{this.props.age}</b> years old</p>
                 <input type="text" onChange={this.props.onChange} value={this.props.name}/>
-            </div>
+            </Aux>
         );
     }
 
 };
 
-export default Person;
+export default wrapperWithClass(styles.Person,Person);
